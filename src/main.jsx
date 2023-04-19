73f09c029ea727ff45e947a9a1b93e8a864d6de9
oncepk <plustor9.ss@gmail.com>
2023-04-19 14:24:20 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./PageHome";
+import Owner from "./PageOwner";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/owner" element={<Owner />} />
+    </Routes>
+  </Router>
+);
